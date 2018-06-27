ff02b87cb244747d2e3a40c2b1c90c366b3b0e61
Mukesh <reghu.mukesh@gmail.com>
2018-06-27 10:48:41 +0530

    Add Codes from Angular Tutorial
    Added Files
    - Routing Module
    - Shared Messaging Service
    - Hero-detail Component
    - Hero Service Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  	this.router.events
+  		.subscribe((event) => {
+  			if (event instanceof NavigationEnd)
+  			{	
+  				let quicklinks_data = {
+					  					quicklinks: {
+					  									url: window.location.href
+					  								}
+  									}
+
+  				//parent.postMessage(this.router.url, '*');
+ 	 			parent.postMessage(quicklinks_data, '*');
+ 	 			//parent.postMessage(window.location.href, '*');
+ 	 			//alert(window.location.href);
+  			}
+ 	 			
+  		});
+  }
+
 }
